3c21830a95a033224a0695e3bfc5cfaa8f0a3454
Francis Penetrante <fcpenet@gmail.com>
2014-01-21 04:25:42 -0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the correct title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
   
   describe "Help Page" do
@@ -15,12 +20,22 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Help')
+    end
   end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About')
     end
   end
 end
